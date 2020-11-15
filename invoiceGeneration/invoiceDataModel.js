949202cb85befe45cc961cf96354be663c49fc04
chunyukuo88 <alexgochenour@gmail.com>
2020-11-15 13:14:12 -0500

    Moved everything into a React project.

---
+++ invoiceGeneration/invoiceDataModel.js
@@ -0,0 +1,17 @@
+const invoiceData = {
+    invoiceNumber: 0,
+    customer: {
+        company: 'Equality Translation Services, Inc.',
+        address: '600 Beaver Street 2C Sewickley PA 15143 USA',
+        name: 'John Cartus',
+        phone: '+412.749.9100'
+    },
+    task: {
+        documentsForReview: '',
+        reviewRate: 36,
+        reviewTime: 1,
+        completionTime: {},
+    }
+}
+
+module.exports = invoiceData;
